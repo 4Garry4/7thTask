c55b326c95356472263c328019311cd409230015
4garry4 <regionhia@gmail.com>
2024-08-25 19:55:40 +0600

    src/

---
+++ src/main/java/ru/netology/mvnQA84/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.mvnQA84;
+
+public class FormDate {
+    int day;
+    int month;
+    int year;
+}
---
+++ src/main/java/ru/netology/mvnQA84/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.mvnQA84;
+
+public class Post {
+    public String name;
+    String passport;
+    String patronymic;
+    String phone;
+    String surname;
+    boolean subscription;
+    FormDate birthday;
+}
